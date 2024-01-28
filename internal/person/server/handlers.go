8562e34052e89ec605e3f9ced9562a2614112afc
raphhawk <bing619.at@gmail.com>
2024-01-28 23:23:38 +0530

    person init

--- internal/person/server/handlers.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import "github.com/labstack/echo/v4"
-
-func GetPerson(ctx echo.Context) error    { return nil }
-func GetPeople(ctx echo.Context) error    { return nil }
-func AddPerson(ctx echo.Context) error    { return nil }
-func UpdatePerson(ctx echo.Context) error { return nil }
-func RemovePerson(ctx echo.Context) error { return nil }
