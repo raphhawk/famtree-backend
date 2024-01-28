33a8c51ed6034fdf32a3db2ed4f3a57696be5501
raphhawk <bing619.at@gmail.com>
2024-01-28 23:24:26 +0530

    person init

---
+++ internal/person/ports/data.go
@@ -0,0 +1,41 @@
+package ports
+
+import (
+	"time"
+
+	artErr "github.com/raphhawk/famtree/cmd/common/error"
+)
+
+// controller contracts
+var (
+	//GET
+	GetPerson = "/api/person/:id"
+	GetPeople = "/api/people"
+
+	//POST
+	AddPerson    = "/api/person"
+	UpdatePerson = "/api/person/:id"
+	RemovePerson = "/api/person/:id"
+)
+
+// PersonDTO acts as data transfer object for Person
+type PersonDTO struct {
+	ID        int                     `json:"id"`
+	FirstName string                  `json:"f_name"`
+	LastName  string                  `json:"l_name"`
+	Gender    string                  `json:"gender"`
+	Age       int                     `json:"age"`
+	Dob       string                  `json:"dob"`
+	Email     string                  `json:"email"`
+	Info      artErr.ArtificialErrors `json:"info"`
+}
+
+// PersonDAO acts as data access object for Person
+type PersonDAO struct {
+	PersonId  int       `json:"p_id"`
+	Name      string    `json:"name"`
+	Dob       string    `json:"dob"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"c_at"`
+	UpdatedAt time.Time `json:"u_at"`
+}
---
+++ internal/person/ports/ports.go
@@ -0,0 +1,28 @@
+package ports
+
+import "database/sql"
+
+type PersonService interface {
+	CreatePerson(person PersonDTO) PersonDTO
+	GetPersonById(id int) PersonDTO
+	GetPersonByEmail(email string) (PersonDTO, error)
+	UpdatePersonName(person PersonDTO) PersonDTO
+	UpdatePersonEmail(person PersonDTO) PersonDTO
+	UpdatePersonDob(person PersonDTO) PersonDTO
+	UpdatePersonGender(person PersonDTO) PersonDTO
+	DeletePerson() error
+	SetAge() error
+	GetAge() (int, error)
+}
+
+type PersonDatabase interface {
+	OpenDB(dsn string) (*sql.DB, error)
+	ConnectToDB() *sql.DB
+	InitDB() error
+	GetDB() *sql.DB
+}
+
+type PersonServer interface {
+	MapUrls() error
+	ListenAndServe() error
+}
